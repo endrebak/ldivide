8e39d9763e611d0d88480192635270df3441e21a
Endre Bakken Stovner <endrebak85@gmail.com>
2020-02-06 15:53:51 +0100

    Gotten further

--- test_pyramid.py
+++ test_pyramid.py
@@ -1,6 +1,3 @@
-"""Just a sanity-check for the code in calc_covar.pyx"""
-
-
 half_window_size = 3
 len_haps = 10
 
@@ -38,3 +35,25 @@ for i in range(len_haps - half_window_size, len_haps):
                 break
 
             print(i, j1, j2)
+
+# 1 1 1
+# 2 2 2
+# 2 1 3
+# 3 3 3
+# 3 2 4
+# 3 1 5
+# 4 4 4
+# 4 3 5
+# 4 2 6
+# 5 5 5
+# 5 4 6
+# 5 3 7
+# 6 6 6
+# 6 5 7
+# 6 4 8
+# 7 7 7
+# 7 6 8
+# 7 5 9
+# 8 8 8
+# 8 7 9
+# 9 9 9

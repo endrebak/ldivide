d01fe764c5129d3124313ab9856dbd39c95c0cc1
Endre Bakken Stovner <endrebak85@gmail.com>
2020-02-11 12:06:09 +0100

    fix to parquet

--- to_parquet.py
+++ to_parquet.py
@@ -8,5 +8,6 @@ df = pd.read_table(stdin, header=None, dtype=np.int8, sep="\s+", index_col=0)
 invalid_haplos = (df != 1) | (df != 0)
 df = df[~invalid_haplos.any(axis=1)]
 df.columns = df.columns.astype(str)
+df.index.name = "Position"
 
 df.to_parquet(outfile)
